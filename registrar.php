d866810e4662a3924f39184793666d9866bc59e5
AlexPartida <aparcab716a@ieslacampina.es>
2023-02-21 14:42:31 +0100

    seugundo commit

--- registrar.php
+++ registrar.php
@@ -9,3 +9,25 @@
 		<style>
 		</style>
 	</head>
+    <body>
+		<div class="container">
+			<div class="row">
+				<h1>Socios</h1>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-8">
+					<form action="registrar2.php" method="get" id="registro" name="registro" autocomplete="off">
+						<div class="form-group">
+							<label for="nombre">Nombre: </label>
+							<input type="text" id="nombre" name="nombre" class="form-control">
+						</div>
+						<div class="form-group">
+						<label for="telefono">Teléfono: </label>
+							<input type="text" id="telefono" name="telefono" maxlength="9" class="form-control">
+						</div>
+						<div class="form-group">
+							<label for="fecha-nac">Fecha de nacimiento: </label>
+							<input type="date" id="fecha-nac" name="fecha-nac" class="form-control">
+						</div>
+						<div class="form-group">
